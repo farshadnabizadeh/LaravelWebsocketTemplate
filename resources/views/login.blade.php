e388f494c0350ee752aae0e08cd1730252bd5b81
Farshad Nabizadeh <farshadn@hotelistan.com>
2024-03-08 12:32:00 +0300

    UPDATED

---
+++ resources/views/login.blade.php
@@ -0,0 +1,4 @@
+@extends('master')
+@section('title','Login')
+@section('body')
+@endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat</title>
+    <title>@yield('title')</title>
     <!-- CSS only -->
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/fonts/circular-std/style.css')}}">
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="{{ asset('vendor/fonts/fontawesome/css/fontawesome-all.css') }}">
 </head>
 <body>
+    @yield('body')
     <script src="{{ asset('vendor/jquery/jquery-3.3.1.min.js') }}"></script>
     <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.js') }}"></script>
     <script src="{{ asset('/scripts/index.js') }}"></script>
