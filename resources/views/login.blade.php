895106b6b715c40a91b11a080cb070258e09c995
Farshad Nabizadeh <farshadn@hotelistan.com>
2024-03-08 14:19:55 +0300

    UPDATED

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,4 +1,35 @@
 @extends('master')
 @section('title','Login')
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/Login.css') }}" />
+@endsection
 @section('body')
+<div class="splash-container">
+    <div class="card ">
+        <div class="card-header text-center"><a href="{{ route('Login') }}"><img class="logo-img" src="{{ asset('images/logo.png') }}" alt="logo"></a><span class="splash-description">Please enter your user information.</span></div>
+        <div class="card-body">
+            <form>
+                <div class="form-group">
+                    <input class="form-control form-control-lg" id="email" type="email" placeholder="Email" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <input class="form-control form-control-lg" id="password" type="password" placeholder="Password">
+                </div>
+                <div class="form-group">
+                    <label class="custom-control custom-checkbox">
+                        <input class="custom-control-input" type="checkbox"><span class="custom-control-label">Remember Me</span>
+                    </label>
+                </div>
+                <button type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button>
+            </form>
+        </div>
+        <div class="card-footer bg-white p-0  ">
+            <div class="card-footer-item card-footer-item-bordered">
+                <a href="#" class="footer-link">Create An Account</a></div>
+            <div class="card-footer-item card-footer-item-bordered">
+                <a href="#" class="footer-link">Forgot Password</a>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,11 +9,12 @@
     <link rel="stylesheet" href="{{ asset('vendor/fonts/circular-std/style.css')}}">
     <link rel="stylesheet" href="{{ asset('libs/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/fonts/fontawesome/css/fontawesome-all.css') }}">
+    @yield('css')
 </head>
 <body>
     @yield('body')
     <script src="{{ asset('vendor/jquery/jquery-3.3.1.min.js') }}"></script>
     <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.js') }}"></script>
-    <script src="{{ asset('/scripts/index.js') }}"></script>
+    <script src="{{ asset('scripts/index.js') }}"></script>
 </body>
 </html>
